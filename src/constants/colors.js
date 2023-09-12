ca419a01958e080688745180b1534f159fee7038
Zakir Bangash <engrzakirbangash@gmail.com>
2023-09-13 04:35:01 +0500

    colors and constants

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const PrimaryDark = '#121212';
+export const SecondryDark = '#000000';
+export const GreyColor = '#aaaa';
+export const PrimaryWhite = '#ffff';
+export const SecondryWhite = '#ddd';
---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+export const PROFILE = {
+  posts: 'Posts',
+  followers: 'Followers',
+  following: 'Following',
+  inDarkMode: 'in Dark Mode',
+  AppPage: 'App Page',
+  officialAccount: 'Official Account',
+  email: 'zbangash191@gmail.com',
+  website: 'thehexatown.com',
+  followedBy: 'followed by',
+  message: 'Message',
+  contact: 'Contact',
+};
