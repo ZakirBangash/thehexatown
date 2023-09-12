601d222f8d1c392bff9b457b03632310e56e4ad6
Zakir Bangash <engrzakirbangash@gmail.com>
2023-09-12 21:59:58 +0500

    navigation

---
+++ src/navigation/BottomTabsNavigation.js
@@ -0,0 +1,36 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import AntDesign from 'react-native-vector-icons';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+      <AntDesign name="stepforward" size={49} color="red" />
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabsNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabsNavigation;
+
+const styles = StyleSheet.create({});
---
+++ src/navigation/index.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BottomTabsNavigation from './BottomTabsNavigation';
+
+const Stack = createNativeStackNavigator();
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="BottomTabNavigation"
+          options={{
+            headerShown: false,
+          }}
+          component={BottomTabsNavigation}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
